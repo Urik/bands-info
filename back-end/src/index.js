49e33e2988ef6b3dccd2f0f41cf46b033d521716
Uri Berman <UriEzequiel.Kleiner@varian.com>
2019-01-04 17:11:37 -0600

    Improving the index.html provisioning

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,16 +1,21 @@
+const path = require('path');
 const express = require('express');
 const bandInfoGetter = require('./bandInfoGetter');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static('../../front-end/dist/angular-bands'));
 
 app.get('/api/band/:bandName', async (req, res, next) => {
     const bandData = await bandInfoGetter.getBandInfo(req.params.bandName);
     res.json(bandData);
     res.end();
 });
-app.use(express.static('../../front-end/dist/angular-bands'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../front-end/dist/angular-bands/index.html'));
+})
 
 app.listen(3000, () => {
     console.log("TEST");
